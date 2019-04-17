5681ec2dd65e0817c04ce2b651373ba70a06d4f0
Fenja Kollasch <kollasch@uni-bremen.de>
2019-04-17 18:14:42 +0200

    Hotfix perception server

--- src/perception_server.cpp
+++ src/perception_server.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
    ros::init(argc, argv, "hsr_perception");
    ros::MultiThreadedSpinner spinner(4);
    ShelfStatusServer shelf_status_server("analyze_shelf_status", "/home/suturo/Desktop/temp");
-   //ObjectInformationServer object_info_server("extract_object_infos", "/home/suturo/Desktop/temp");
+   ObjectInformationServer object_info_server("extract_object_infos", "/home/suturo/Desktop/temp");
    spinner.spin();
 
    return 0;
